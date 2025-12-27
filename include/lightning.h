716bf2104544e8cfa5fc896fb6154c12878a3773
tstwroot <tstwroot@protonmail.com>
2025-12-27 02:15:34 -0400

    Add: Lightning umbrella header

---
+++ include/lightning.h
@@ -0,0 +1,32 @@
+/**
+  * Lightning - Web Framework.
+  * Copyright (C) 2025  Pablo Vitorino Panciera (tstwroot) <tstwroot@protonmail.com>
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  */
+
+/**
+ * @file lightning.h
+ * @brief Lightning Web Framework - Main Header
+ *
+ * This is the umbrella header that includes all Lightning modules.
+ * For most use cases, simply include this file:
+ *
+ *   #include <lightning.h>
+ */
+
+#ifndef LIGHTNING_H
+#define LIGHTNING_H
+//      LIGHTNING_H
+#endif
